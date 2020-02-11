dcf9660e26188c6d24d278ed1452bf4750d2e6f4
Patrice H <patrice@hannotin.fr>
2020-02-11 13:42:19 -1000

    Add fonts

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	export let name;
+	let animate;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 class="{animate ? `animate` : ''}">Hello {name}!</h1>
+	<label>
+		<input type=checkbox bind:checked={animate}>
+		Animate
+	</label>
 </main>
 
 <style>
@@ -15,12 +19,38 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+h1 {
+	margin: 0;
+	font-size: 20vw;
+	position: relative;
+	font-weight: 400;
+	font-family: 'Decovar';
+	/* font-family: 'Montserrat'; */
+	/* font-family: 'AmstelvarAlpha'; */
+
+	/* // Background clip to image */
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/209708058_b5a5fb07a6_o.jpg);
+	background-size: 30%;
+	background-repeat: repeat;
+
+	text-shadow: 2px 2px 5px rgba(#2a4308, 0.4);
+}
+
+.animate {
+  animation: grow 2s infinite alternate-reverse;
+}
+
+ @keyframes grow {
+	0% {
+		font-variation-settings: 'wght' 10, 'wdht' 10, 'slnt' 10, 'INLN' 10, 'SWRM' 0;
 	}
+
+	100% {
+		font-variation-settings: 'wght' 0, 'wdht' 1500, 'slnt' 1500, 'INLN' 500, 'SWRM' 1000;
+	}
+}
 
 	@media (min-width: 640px) {
 		main {
